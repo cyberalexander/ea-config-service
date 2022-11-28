37108a713be9d0baf093dc91b679c486bf9817a9
CyberAlexander <taurum13@gmail.com>
2022-11-28 09:46:38 +0300

    Project implementation and docker configurations are added to the structure

---
+++ src/test/java/com/eagle/eye/config/EaConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.eye.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
