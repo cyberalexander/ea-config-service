598138b5e954e3c0fc7549c974cd215004008fc8
CyberAlexander <taurum13@gmail.com>
2023-03-21 09:25:09 +0300

    Config Service Spring Config updated

--- src/main/java/com/eagle/eye/config/SecurityConfiguration.java
+++ src/main/java/com/eagle/eye/config/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 cyberalexander
+ * Copyright (c) 2022-2023 CyberAlexander
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@ import org.springframework.context.annotation.Configuration;
  * Project : ea-config-service
  * IDE : IntelliJ IDEA
  *
- * @author Aliaksandr_Leanovich
+ * @author CyberAlexander
  * @version 1.0
  */
 @Configuration
